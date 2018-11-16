c2176fc04a8404775efd264fd05a210f6beb1c3b
Elias Hanna <h.elias@hotmail.fr>
2018-11-16 17:30:43 +0100

    Final version 1.0

--- src/teleop.cpp
+++ src/teleop.cpp
@@ -8,19 +8,27 @@ void joyCallback(const sensor_msgs::Joy::ConstPtr& msg)
 {
 	linear_speed = msg->axes[1];
 	angular_speed = msg->axes[0];
+	std::cout << linear_speed << " | " << angular_speed << std::endl;
 }
 
 int main(int argc, char* argv[])
 {
+	if(argc != 2)
+	{
+		std::cout << "Usage: teleop <topicToTeleoperate>" << std::endl;
+		return 1;
+	}
+
 	ros::init(argc, argv, "teleop");
 	ros::NodeHandle n;
 
-	ros::Subscriber subJoy = n.subscribe("joy", 1000, joyCallback);
-	ros::Publisher pubSim = n.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1000);
+	ros::Subscriber subJoy = n.subscribe("/joy", 10, joyCallback);
+	ros::Publisher pubSim = n.advertise<geometry_msgs::Twist>(argv[1], 10);
 	ros::Rate loop_rate(rate);
 
 	while(ros::ok())
 	{
+		ros::spinOnce();
 		geometry_msgs::Twist cmd_vel;
 		cmd_vel.linear.x = linear_speed;
 		cmd_vel.angular.z = angular_speed;
