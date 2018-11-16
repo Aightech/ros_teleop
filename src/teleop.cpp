265f2f8533ce4962ce9142832f620c0125636cd8
Elias Hanna <h.elias@hotmail.fr>
2018-11-16 16:02:00 +0100

    Changes to teleop.cpp

--- src/teleop.cpp
+++ src/teleop.cpp
@@ -1,9 +1,15 @@
 #include <teleop.hpp>
 
+void joyCallback(const geometry_msgs::Twist& msg)
+{
+	
+}
+
 int main(int argc, char* argv[])
 {
 	ros::init(argc, argv, "teleop");
 	ros::NodeHandle n;
-	
+
+	ros::Subscriber subJoy = n.subscribe("joy", 1000, joyCallback);
 	return 0;
 }
