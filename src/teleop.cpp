0588521c0bfaedc84ef3a264f144ba07335f258d
Alexis Devillard <aiiight.tech@gmail.com>
2018-12-20 19:03:02 +0100

    update

--- src/teleop.cpp
+++ src/teleop.cpp
@@ -1,37 +1,32 @@
 #include "teleop/teleop.hpp"
+#include "std_msgs/Float32.h"
 
 int rate = 10; //10 Hz
-float linear_speed = 0.0;
-float angular_speed = 0.0;
+std_msgs::Float32 right_speed;
+std_msgs::Float32 left_speed;
 
 void joyCallback(const sensor_msgs::Joy::ConstPtr& msg)
 {
-	linear_speed = msg->axes[1];
-	angular_speed = msg->axes[0];
+	right_speed.data = msg->axes[1]*0.8;
+	left_speed.data = msg->axes[4]*0.8;
 }
 
 int main(int argc, char* argv[])
 {
-	if(argc != 2)
-	{
-		std::cout << "Usage: teleop <topicToTeleoperate>" << std::endl;
-		return 1;
-	}
-
 	ros::init(argc, argv, "teleop");
 	ros::NodeHandle n;
 
 	ros::Subscriber subJoy = n.subscribe("/joy", 10, joyCallback);
-	ros::Publisher pubSim = n.advertise<geometry_msgs::Twist>(argv[1], 10);
+	ros::Publisher pubRight = n.advertise<std_msgs::Float32>("/simu_fastsim/speed_left", 10);
+	ros::Publisher pubLeft =  n.advertise<std_msgs::Float32>("/simu_fastsim/speed_right", 10);
 	ros::Rate loop_rate(rate);
 
 	while(ros::ok())
 	{
 		ros::spinOnce();
-		geometry_msgs::Twist cmd_vel;
-		cmd_vel.linear.x = linear_speed;
-		cmd_vel.angular.z = angular_speed;
-		pubSim.publish(cmd_vel);
+		
+		pubRight.publish(right_speed);
+		pubLeft.publish(left_speed);
 	}
 	return 0;
 }
